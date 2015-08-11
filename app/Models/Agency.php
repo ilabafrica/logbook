2b3b9e7c3356e0eb05bfa88d92960e17b99245c9
piusyo <pmpiusyo@gmail.com>
2015-08-11 16:47:06 +0300

    Audit trails updates

--- app/Models/Agency.php
+++ app/Models/Agency.php
@@ -2,9 +2,26 @@
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Sofa\Revisionable\Laravel\RevisionableTrait; // trait
+use Sofa\Revisionable\Revisionable; // interface
 
 class Agency extends Model {
     use SoftDeletes;
     protected $dates = ['deleted_at'];
 	protected $table = 'agencies';
+}
+class Agency extends Model implements Revisionable {
+    use SoftDeletes;
+    protected $dates = ['deleted_at'];
+	protected $table = 'agencies';
+	use RevisionableTrait;
+
+    /*
+     * Set revisionable whitelist - only changes to any
+     * of these fields will be tracked during updates.
+     */
+    protected $revisionable = [
+        'name',
+        'description',
+    ];
 }
